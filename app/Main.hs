24c861ba35382f0beba0928b51e18e8064d21918
Kojo Bailey <kojomolojo@gmail.com>
2025-11-28 01:09:29 +0000

    add conversion from rgba vector to list of pixels

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,11 @@
 
 module Main where
 
-import Control.Monad
-
+import qualified Data.Vector.Storable as S
 import Codec.Picture
-import Codec.Picture.Types (pixelFold)
 
--- data Pixel = RGB Float Float Float | RGBA Float Float Float Float
+data Pixel = RGB Float Float Float | RGBA Float Float Float Float
+  deriving (Show)
 
 -- type ImageData = [Pixel]
 
@@ -28,14 +27,31 @@ get_width =
   getLine >>= \input ->
   pure $ if null input then 80 else (read input :: Int)
 
+get_image_data :: String -> IO (Either String (S.Vector (PixelBaseComponent PixelRGBA8)))
+get_image_data path = 
+  readImage path >>= \result ->
+  pure $ case result of
+    Left err      -> Left err
+    Right dynImg  -> Right $ imageData (convertRGBA8 dynImg)
+
+convert_rec :: S.Vector (PixelBaseComponent PixelRGBA8) -> Int -> [Main.Pixel]
+convert_rec v i = RGBA r g b a : convert_rec v (i+4)
+  where
+    r = fromIntegral (v S.! i) / 255
+    g = fromIntegral (v S.! (i+1)) / 255
+    b = fromIntegral (v S.! (i+2)) / 255
+    a = fromIntegral (v S.! (i+3)) / 255
+
+convert :: S.Vector (PixelBaseComponent PixelRGBA8) -> [Main.Pixel]
+convert v = convert_rec v 0 
+
+
 main :: IO ()
 main =
   get_width >>= \width ->
   print width >>
   putStrLn "Converting image to ASCII..." >>
   let imgPath = "C:/Users/kojom/Downloads/7c1b1215-a00a-4ef7-8331-e2dfab6f5af7.jpg" in
-  readImage imgPath >>= \case
-    Left err -> putStrLn $ "Error: " ++ err
-    Right dynImg ->
-      let rgbaImg = convertRGBA8 dynImg in
-      print $ imageData rgbaImg
+  get_image_data imgPath >>= \case
+    Left err      -> putStrLn $ "Error: " ++ err
+    Right dynImg  -> print $ convert dynImg
