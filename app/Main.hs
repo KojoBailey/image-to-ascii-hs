b1e9d207a170c2dd9326e8c542a33c77fa0c604a
Kojo Bailey <kojomolojo@gmail.com>
2025-11-29 02:06:16 +0000

    Use vector for pixels for faster performance

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,33 @@ module Main where
 import qualified Data.Vector.Storable as S
 import Control.Monad
 import qualified Codec.Picture as Juicy
+import Foreign.Storable
 
 data Pixel = RGBA Float Float Float Float
   deriving (Show)
 
-type Pixels = [Pixel]
+instance Storable Pixel where
+  sizeOf _ = 16
+  alignment _ = alignment (undefined :: Float)
+  peek ptr =
+    peekByteOff ptr 0 >>= \r ->
+    peekByteOff ptr 4 >>= \g ->
+    peekByteOff ptr 8 >>= \b ->
+    peekByteOff ptr 12 >>= \a ->
+    pure (RGBA r g b a)
+  poke ptr (RGBA r g b a) =
+    pokeByteOff ptr 0 r >>
+    pokeByteOff ptr 4 g >>
+    pokeByteOff ptr 8 b >>
+    pokeByteOff ptr 12 a 
+
+type Pixels = S.Vector Pixel
 
 data Image = Image {
   pixels :: Pixels,
   width :: Int,
   height :: Int
-  } deriving (Show)
+  }
 
 min3 :: Ord a => a -> a -> a -> a
 min3 a b = min (min a b)
@@ -47,10 +63,10 @@ get_image path =
 
 clamp_x_rec :: Int -> Float -> Int -> Int -> Float -> Image -> Pixels
 clamp_x_rec column step new_width y_index step_size img
-  | column >= new_width = []
+  | column >= new_width = S.empty
   | otherwise       =
-    pixels img !! (y_index * width img + x_index)
-      : clamp_x_rec (column+1) (step + step_size) new_width y_index step_size img
+    pixels img S.! (y_index * width img + x_index)
+      `S.cons` clamp_x_rec (column+1) (step + step_size) new_width y_index step_size img
   where
     x_index = if (column+1) == new_width then width img - 1 else floor step
 
@@ -59,10 +75,10 @@ clamp_x = clamp_x_rec 0 0
 
 clamp_y_rec :: Int -> Float -> Int -> Int -> Float -> Float -> Image -> Pixels
 clamp_y_rec row step new_height new_width y_step_size x_step_size img
-  | row >= new_height = []
+  | row >= new_height = S.empty
   | otherwise     =
     clamp_x new_width y_index x_step_size img
-      ++ clamp_y_rec (row+1) (step + y_step_size) new_height new_width y_step_size x_step_size img
+      S.++ clamp_y_rec (row+1) (step + y_step_size) new_height new_width y_step_size x_step_size img
   where
     y_index = if (row+1) == new_height then height img - 1 else floor step
 
@@ -83,7 +99,9 @@ clamp max_width img =
     y_step_size = fromIntegral (height img) / fromIntegral new_height
 
 convert_rec :: Int -> S.Vector (Juicy.PixelBaseComponent Juicy.PixelRGBA8) -> Pixels
-convert_rec i v = RGBA r g b a : convert_rec (i+4) v
+convert_rec i v
+  | i >= S.length v = S.empty
+  | otherwise           = RGBA r g b a `S.cons` convert_rec (i+4) v
   where
     r = fromIntegral (v S.! i) / 255
     g = fromIntegral (v S.! (i+1)) / 255
@@ -96,18 +114,19 @@ convert img = Image {
   width = Juicy.imageWidth img,
   height = Juicy.imageHeight img }
 
-print_ascii_rec :: Int -> Int -> [Char] -> IO ()
-print_ascii_rec _ _ [] = pure ()
-print_ascii_rec i w (c:cs) =
-  putChar c >>
-  when (i > 0 && (i+1) `mod` w == 0) (putChar '\n') >>
-  print_ascii_rec (i+1) w cs
+print_ascii_rec :: Int -> Int -> S.Vector Char -> IO ()
+print_ascii_rec i w cs
+  | i + 1 >= S.length cs = pure ()
+  | otherwise            =
+    putChar (S.head cs) >>
+    when (i > 0 && (i+1) `mod` w == 0) (putChar '\n') >>
+    print_ascii_rec (i+1) w (S.tail cs)
 
 print_ascii :: Juicy.Image Juicy.PixelRGBA8 -> Int -> IO ()
 print_ascii orig_img max_width = print_ascii_rec 0 (width img) ascii
   where
     img = clamp max_width (convert orig_img)
-    ascii = map to_ascii (pixels img)
+    ascii = S.map to_ascii (pixels img)
 
 main :: IO ()
 main =
